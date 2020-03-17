9417b13844bec31e5df681c86e66c6c2d2970999
SnowBlack1 <varvoux.aurelie@gmail.com>
2020-03-17 13:30:10 +0100

    Layout objects (txtview/FAB/imgview) instantiated in Java class

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/SpecificNeighbour.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/SpecificNeighbour.java
@@ -1,15 +1,50 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.openclassrooms.entrevoisins.R;
 
 public class SpecificNeighbour extends AppCompatActivity {
-//ACTIVITE POUR PROJET
+
+    private ImageView avatar;
+    private TextView nameAvatar;
+    private TextView nameTitle;
+    private TextView adress;
+    private TextView phone;
+    private TextView social;
+    private TextView aboutMeTitle;
+    private TextView aboutMeText;
+    private FloatingActionButton favBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_neighbour);
+
+
+
+        avatar = findViewById(R.id.avatar_img);
+
+        nameAvatar = findViewById(R.id.nameavatar_txt);
+        //nameAvatar.setText();
+
+        nameTitle = findViewById(R.id.name1_txt);
+
+        adress = findViewById(R.id.address_txt);
+
+        phone = findViewById(R.id.phone_txt);
+
+        social = findViewById(R.id.social_txt);
+
+        aboutMeTitle = findViewById(R.id.aboutme_title);
+
+        aboutMeText = findViewById(R.id.aboutme_txt);
     }
+
+
 }
