d8abf55dc6bd2bd2cf57927eb15a4c78c7ad7e13
SnowBlack1 <varvoux.aurelie@gmail.com>
2020-04-03 12:24:53 +0200

    Resolution of avatarURL changed -> 150 to 300

--- app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourGenerator.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourGenerator.java
@@ -9,40 +9,40 @@ import java.util.List;
 public abstract class DummyNeighbourGenerator {
 
     public static List<Neighbour> DUMMY_NEIGHBOURS = Arrays.asList(
-            new Neighbour(1, "Caroline", "https://i.pravatar.cc/150?u=a042581f4e29026704d", "Saint-Pierre-du-Mont, 5km", "+33 6 86 57 90 14", "www.facebook.fr/Caroline",
+            new Neighbour(1, "Caroline", "https://i.pravatar.cc/300?u=a042581f4e29026704d", "Saint-Pierre-du-Mont, 5km", "+33 6 86 57 90 14", "www.facebook.fr/Caroline",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(2, "Jack", "https://i.pravatar.cc/150?u=a042581f4e29026704e", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Jack",
+            new Neighbour(2, "Jack", "https://i.pravatar.cc/300?u=a042581f4e29026704e", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Jack",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(3, "Chloé", "https://i.pravatar.cc/150?u=a042581f4e29026704f", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Chloé",
+            new Neighbour(3, "Chloé", "https://i.pravatar.cc/300?u=a042581f4e29026704f", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Chloé",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(4, "Vincent", "https://i.pravatar.cc/150?u=a042581f4e29026704a", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Vincent",
+            new Neighbour(4, "Vincent", "https://i.pravatar.cc/300?u=a042581f4e29026704a", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Vincent",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(5, "Elodie", "https://i.pravatar.cc/150?u=a042581f4e29026704b", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Elodie",
+            new Neighbour(5, "Elodie", "https://i.pravatar.cc/300?u=a042581f4e29026704b", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Elodie",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(6, "Sylvain", "https://i.pravatar.cc/150?u=a042581f4e29026704c", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Sylvain",
+            new Neighbour(6, "Sylvain", "https://i.pravatar.cc/300?u=a042581f4e29026704c", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Sylvain",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(7, "Laetitia", "https://i.pravatar.cc/150?u=a042581f4e29026703d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Laetitia",
+            new Neighbour(7, "Laetitia", "https://i.pravatar.cc/300?u=a042581f4e29026703d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Laetitia",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(8, "Dan", "https://i.pravatar.cc/150?u=a042581f4e29026703b", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Dan",
+            new Neighbour(8, "Dan", "https://i.pravatar.cc/300?u=a042581f4e29026703b", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Dan",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(9, "Joseph", "https://i.pravatar.cc/150?u=a042581f4e29026704d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Joseph",
+            new Neighbour(9, "Joseph", "https://i.pravatar.cc/300?u=a042581f4e29026704d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Joseph",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(10, "Emma", "https://i.pravatar.cc/150?u=a042581f4e29026706d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Emma",
+            new Neighbour(10, "Emma", "https://i.pravatar.cc/300?u=a042581f4e29026706d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Emma",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initiée, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(11, "Patrick", "https://i.pravatar.cc/150?u=a042581f4e29026702d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Patrick",
+            new Neighbour(11, "Patrick", "https://i.pravatar.cc/300?u=a042581f4e29026702d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Patrick",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot.."),
 
-            new Neighbour(12, "Ludovic", "https://i.pravatar.cc/150?u=a042581f3e39026702d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Ludovic",
+            new Neighbour(12, "Ludovic", "https://i.pravatar.cc/300?u=a042581f3e39026702d", "Saint-Pierre-du-Mont ; 5km", "+33 6 86 57 90 14", "www.facebook.fr/Ludovic",
                     "A propos de moi", "Bonjour !Je souhaiterais faire de la marche nordique. Pas initié, je recherche une ou plusieurs personnes susceptibles de m'accompagner ! J'aime les jeux de cartes tels la belote et le tarot..")
     );
 
